9e720e46d7e27b9efb541370957ebcf0c6558668
neo2k17 <33368984+neo2k17@users.noreply.github.com>
2017-11-04 16:23:57 +0700

    Add files via upload

--- src/bot.js
+++ src/bot.js
@@ -1,20 +1,12 @@
 const LineConnect = require('./connect');
 let LINE = require('./main.js');
-console.info("\n\
-=========================================\n\
-BotName: LINE Etot JS\n\
-Version: 0.2.1\n\
-Terima Kasih Kepada @Alfathdirk @TCR_TEAM\n\
-=========================================\n\
-\nNOTE : Ini Adalah AlphatJS Lama Buatan @Alfathdirk @TCR_TEAM Dan Ini Telah Dikembangin Oleh @TAB_TEAM\nTolong Untuk Tidak Perjual-Belikan Script Ini!\n\
-****Nekopoi.host Running****");
 
 const auth = {
-	authToken: 'EmnAJjvLzLfEIxQkCvnb.5QT9zEniAkpIZc1LxU3YwW.0Pv2nbBsi0qguWRNmOTqrYYZJ4XLkeMKH070Gd7fa/o=',
-	certificate: 'a82e8634329485d3fdf0166b9741b6f68aa02073387211c9c70914778ba20c3a',
+	authToken: 'EmFcAwCbnFybbGzry2s2.isi7lEmjXqxFrk0Elt+K8G.U8cbAzEb76BhW+5kGm6EJM8UNzm89Jz5lTWou2xvmYo=',
+	certificate: '7268ab9fd5290d9402b1118544a2d3ca8d14e17edc2fc29d32e7c2736d002147',
 }
 let client =  new LineConnect(auth);
-// let client =  new LineConnect();
+//let client =  new LineConnect();
 
 client.startx().then(async (res) => {
 	
